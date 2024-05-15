6f55120a342df1ceb63bc9245f06e4f739d83a62
Aya1Khlif <ayakhlife222@gmail.com>
2024-05-15 17:16:26 +0300

    aya

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Comment;
 use App\Models\Document;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,16 +13,11 @@ class CommentController extends Controller
             'content' => 'required|string',
         ]);
 
-        $userId = auth()->id();
+        $comment = $document->comments()->create([
+            'content' => $validatedData['content'],
+            'user_id' => Auth::id(),
+        ]);
 
-// ثم تأكد من تمريره عند إنشاء التعليق
-$comment = new Comment([
-    'content' => $request->input('content'),
-    'user_id' => $userId,
-    'commentable_id' => $request->input('commentable_id'),
-    'commentable_type' => $request->input('commentable_type'),
-]);
-$comment->save();
         return response()->json($comment, 201);
     }
 
